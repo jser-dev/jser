df6188813f1c3b511ee40d1eedd2a7cc1c1d8c5a
Houfeng <admin@xhou.net>
2015-09-15 00:17:06 +0800

    dev

--- controllers/home.js
+++ controllers/home.js
@@ -3,6 +3,6 @@ var Home = module.exports = function() {};
 Home.prototype.index = function() {
     var self = this;
     self.render("home.html", {
-        "name": "Nokit MVC"
+        "name": "JSER.CC"
     });
 };
