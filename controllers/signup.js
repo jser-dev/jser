f3383aed1c8afa43043748c8746ae18fe04009cd
Houfeng <admin@xhou.net>
2015-09-19 00:25:33 +0800

    dev

--- controllers/signup.js
+++ controllers/signup.js
@@ -2,33 +2,28 @@
 
 var User = require('../models/user');
 
+//定义 controller
 var SignUpController = module.exports = function () { };
 
+//默认 action
 SignUpController.prototype.index = function () {
     var self = this;
-    self.render("signup.html", {
-        data: {}
-    });
+    self.render("signup.html");
 };
 
+//提交注册信息
 SignUpController.prototype.submit = function () {
     var self = this;
     var user = self._newUser();
-    if (!self._checkUser(user)) {
-        return self._submitFail("注册失败");
-    }
-    user.save(function (err) {
-        if (err) {
-            self._submitFail("注册失败");
-        } else {
-            self._submitSuccess("注册成功");
-        }
+    User.signUp(user, function (result) {
+        self.render('signup.html', result);
     });
 };
 
+//通过浏览器 post 过来的数据创建一个 “User”
 SignUpController.prototype._newUser = function () {
     var self = this;
-    var user = new User();
+    var user = User.create();
     var req = self.context.request;
     user.account = req.body.account;
     user.email = req.body.email;
@@ -36,27 +31,4 @@ SignUpController.prototype._newUser = function () {
     user.name = req.body.name;
     user.avatar = req.body.avatar;
     return user;
-};
-
-SignUpController.prototype._checkUser = function (user) {
-    var self = this;
-    return user.account && user.email && user.password;
-};
-
-SignUpController.prototype._submitFail = function (message) {
-    var self = this;
-    var req = self.context.request;
-    self.render('signup.html', {
-        status: false,
-        message: message,
-        data: req.body
-    });
-};
-
-SignUpController.prototype._submitSuccess = function (message) {
-    var self = this;
-    self.render('signup.html', {
-        status: true,
-        message: message
-    });
 };
--- models/user.js
+++ models/user.js
@@ -11,11 +11,49 @@ var User = module.exports = db.model('user', {
     avatar: String
 });
 
+//创建一个新用户
+User.create = function () {
+    return new User();
+};
+
 //加载所有用户
 User.load = function (callback) {
     var self = User;
     self.find({}, function (err, list) {
         self.list = list;
         if (callback) callback(err, list);
+    });
+};
+
+//检查注册用户
+User._checkSignUp = function (user) {
+    var self = this;
+    return user.account && user.email && user.password;
+};
+
+//注册一个用户
+User.signUp = function (user, callback) {
+    var self = this;
+    if (!self._checkSignUp(user)) {
+        return callback({
+            status: false,
+            message: '用户信息不合法',
+            data: user
+        });
+    }
+    user.save(function (err) {
+        if (err) {
+            return callback({
+                status: false,
+                message: '发生了异常:' + err,
+                data: user
+            });
+        } else {
+            return callback({
+                status: true,
+                message: '注册成功',
+                data: user
+            });
+        }
     });
 };
