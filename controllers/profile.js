350b74215b00334192e90f47c227c793726b4608
Houfeng <admin@xhou.net>
2015-10-02 10:25:17 +0800

    个人中心菜单响应式处理

--- controllers/profile.js
+++ controllers/profile.js
@@ -8,3 +8,10 @@ ProfileController.prototype.index = function () {
     var self = this;
     self.render("profile.html", {});
 };
+
+ProfileController.prototype.signout = function () {
+    var self = this;
+    self.context.session.add('user', null, function () {
+        self.context.redirect("/");
+    });
+};
