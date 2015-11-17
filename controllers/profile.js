d0246fea541e4d50cf4ac8ffa7b7d3cd45600ec5
Houfeng <admin@xhou.net>
2015-11-17 15:10:44 +0800

    提交个人配置页

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,9 +1,8 @@
-"use strict";
-
 var User = require('../models/user');
 
 var ProfileController = module.exports = function () { };
 
+//默认 action
 ProfileController.prototype.index = function () {
     var self = this;
     self.render("profile.html", {
@@ -11,6 +10,49 @@ ProfileController.prototype.index = function () {
     });
 };
 
+ProfileController.prototype.saveBaseInfo = function () {
+    var self = this;
+    var name = self.context.data("name");
+    var avatar = self.context.request.files["avatar"];
+    User.saveBaseInfo({
+        "id": self.context.user._id,
+        "name": name,
+        "avatar": avatar.size > 0 ? avatar.path : null
+    }, function (err, info) {
+        if (!err && info) {
+            self.context.user.name = info.name || self.context.user.name;
+            self.context.user.avatar = info.avatar || self.context.user.avatar;
+            self.context.session.set("user", self.context.user);
+        }
+        self.render("profile.html", {
+            saveBaseInfoMessage: err || "保存信息完成",
+            user: self.context.user
+        });
+    });
+};
+
+ProfileController.prototype.changePassword = function () {
+    var self = this;
+    var password = self.context.data("password");
+    var confirmPassword = self.context.data("confirmPassword");
+    if (password != confirmPassword) {
+        return self.render("profile.html", {
+            changePasswordMessage: "两次密码不一致",
+            user: self.context.user
+        });
+    }
+    User.setPassword({
+        "id": self.context.user._id,
+        "password": password
+    }, function (err) {
+        self.render("profile.html", {
+            changePasswordMessage: err || "修改密码完成",
+            user: self.context.user
+        });
+    });
+};
+
+//退出
 ProfileController.prototype.signout = function () {
     var self = this;
     self.context.session.remove("user", function () {
--- models/user.js
+++ models/user.js
@@ -1,55 +1,63 @@
 var utils = require("../common/utils");
 var mail = require("./mail");
 var define = require("./define");
+var qn = require("qn");
+var fs = require("fs");
 
 //定义用户模型
 var User = define.User;
 
 //创建一个新用户
-User.create = function () {
+User.create = function() {
     return new User();
 };
 
 //加载按积分降序排序的 n 个用户
-User.getList = function (top, callback) {
+User.getList = function(top, callback) {
     var self = User;
-    self.find({}) 
-        .sort({ 'integral': -1, '_id': 1 })
+    self.find({})
+        .sort({
+            'integral': -1,
+            '_id': 1
+        })
         .limit(top)
         .exec(callback);
 };
 
 //登录一个用户
-User.signIn = function (user, callback) {
+User.signIn = function(user, callback) {
     var self = User;
     if (!user.email || !user.password) {
         return callback("用户或者密码错误");
     }
-    self.findOne({ 
-        "email": new RegExp(user.email,"igm"), 
-        "password": utils.hashDigest(user.password) }, 
-    function (err, foundUser) {
-        if (err) {
-            return callback(err,user); 
-        }
-        if(foundUser && foundUser.verifyCode){
-            return callback("该用户的邮箱还未验证",user);
-        }else if (foundUser) {
-            return callback(null, foundUser);
-        } else {
-            return callback('用户或者密码错误',user);
-        }
-    });
+    self.findOne({
+            "email": new RegExp(user.email, "igm"),
+            "password": utils.hashDigest(user.password)
+        },
+        function(err, foundUser) {
+            if (err) {
+                return callback(err, user);
+            }
+            if (foundUser && foundUser.verifyCode) {
+                return callback("该用户的邮箱还未验证", user);
+            } else if (foundUser) {
+                return callback(null, foundUser);
+            } else {
+                return callback('用户或者密码错误', user);
+            }
+        });
 };
 
 //通过 oauth 认证一个用户
-User.oAuth = function (user, callback) {
+User.oAuth = function(user, callback) {
     var self = User;
     if (!user || !user.email) {
         return callback('oAuth 发生了异常，没有可用 email');
     }
     user.avatar = user.avatar || self.getAvatar();
-    self.findOne({ "email": user.email }, function (err, foundUser) {
+    self.findOne({
+        "email": user.email
+    }, function(err, foundUser) {
         if (err) {
             return callback(err);
         }
@@ -57,7 +65,7 @@ User.oAuth = function (user, callback) {
             return callback(null, foundUser);
         } else {
             user.avatar = user.avatar || self.getAvatar();
-            user.save(function (err) {
+            user.save(function(err) {
                 if (err) {
                     return callback(err);
                 }
@@ -68,11 +76,11 @@ User.oAuth = function (user, callback) {
 };
 
 //根据一个字段检查是否存在用户
-User.existsByField=function(field,value,callback){
+User.existsByField = function(field, value, callback) {
     var self = this;
-    var options={};
-    options[field]=new RegExp(value,"igm");
-    self.findOne(options, function (err, foundUser) {
+    var options = {};
+    options[field] = new RegExp(value, "igm");
+    self.findOne(options, function(err, foundUser) {
         if (err) {
             return callback(err);
         }
@@ -81,7 +89,7 @@ User.existsByField=function(field,value,callback){
 };
 
 //注册一个用户
-User.signUp = function (user, callback) {
+User.signUp = function(user, callback) {
     var self = this;
     user.avatar = user.avatar || self.getAvatar();
     if (!user.email || user.email.indexOf('@') < 0) {
@@ -93,27 +101,27 @@ User.signUp = function (user, callback) {
     if (!user.password || user.password.length < 6) {
         return callback('密码最少需要六个字符');
     }
-    self.existsByField("email",user.email,function(err,existsUser){
-       if(err){
-           return callback(err);
-       } 
-       if(existsUser){
-           return callback('邮箱 "'+user.email+'" 已经被使用');
-       }
-        self.existsByField("name",user.name,function(err,existsUser){
-            if(err){
+    self.existsByField("email", user.email, function(err, existsUser) {
+        if (err) {
+            return callback(err);
+        }
+        if (existsUser) {
+            return callback('邮箱 "' + user.email + '" 已经被使用');
+        }
+        self.existsByField("name", user.name, function(err, existsUser) {
+            if (err) {
                 return callback(err);
-            } 
-            if(existsUser){
-                return callback('名字 "'+user.name+'" 已经被使用');
+            }
+            if (existsUser) {
+                return callback('名字 "' + user.name + '" 已经被使用');
             }
             user.password = utils.hashDigest(user.password);
             user.verifyCode = utils.newGuid();
-            user.save(function (err) {
+            user.save(function(err) {
                 if (err) {
                     return callback(err);
                 }
-                mail.sendForReg(user, function (err) {
+                mail.sendForReg(user, function(err) {
                     if (err) {
                         return callback(err);
                     }
@@ -125,7 +133,7 @@ User.signUp = function (user, callback) {
 };
 
 //生成一个用户头像
-User.getAvatar = function () {
+User.getAvatar = function() {
     var index = utils.random(1, 12);
     return "/images/avatar/" + index + ".png";
 };
@@ -133,7 +141,7 @@ User.getAvatar = function () {
 /**
  * 获取一个用户
  **/
-User.getUser = function (id, callback) {
+User.getUser = function(id, callback) {
     var self = this;
     self.findById(id, callback);
 };
@@ -141,10 +149,18 @@ User.getUser = function (id, callback) {
 /**
  * 搜索匹配的人员
  **/
-User.search = function (keyword, callback) {
+User.search = function(keyword, callback) {
     var self = this;
-    self.find({ "name": { $regex: keyword, $options: 'i' } })
-        .sort({ 'integral': -1, '_id': 1 })
+    self.find({
+            "name": {
+                $regex: keyword,
+                $options: 'i'
+            }
+        })
+        .sort({
+            'integral': -1,
+            '_id': 1
+        })
         .limit(10)
         .exec(callback);
 };
@@ -152,15 +168,17 @@ User.search = function (keyword, callback) {
 /**
  * 验证邮箱
  **/
-User.verifyMail = function (verifyCode, callback) {
+User.verifyMail = function(verifyCode, callback) {
     var self = this;
-    self.findOne({ "verifyCode": verifyCode }, function (err, foundUser) {
+    self.findOne({
+        "verifyCode": verifyCode
+    }, function(err, foundUser) {
         if (err) {
             return callback(err);
         }
         if (foundUser) {
             foundUser.verifyCode = '';
-            foundUser.save(function (err) {
+            foundUser.save(function(err) {
                 if (err) {
                     return callback(err);
                 }
@@ -172,5 +190,98 @@ User.verifyMail = function (verifyCode, callback) {
     });
 };
 
+//设置密码
+User.setPassword = function(opts, callback) {
+    var self = this;
+    if (!opts.password || opts.password.length < 6) {
+        return callback('密码最少需要六个字符');
+    }
+    self.update({
+        "_id": opts.id
+    }, {
+        $set: {
+            password: utils.hashDigest(opts.password)
+        }
+    }, callback);
+};
+
+//初始化七牛
+User._initQiQiu = function() {
+    var self = this;
+    if (!self.quClient) {
+        var qnConfigs = utils.configs.qiniu;
+        self.quClient = qn.create({
+            accessKey: qnConfigs.accessKey,
+            secretKey: qnConfigs.secretKey,
+            bucket: qnConfigs.bucket,
+            origin: qnConfigs.origin
+        });
+    }
+};
+
+User._uploadAvatar = function(baseInfo, callback) {
+    var self = this;
+    self.quClient.delete(baseInfo.id, function(err) {
+        self.quClient.upload(fs.createReadStream(baseInfo.avatar), {
+            key: baseInfo.id
+        }, function(err, result) {
+            if (err) {
+                return callback(err);
+            }
+            //baseInfo.avatar = result.url;
+            baseInfo.avatar = self.quClient.imageView(baseInfo.id, {
+                mode: 1,
+                width: 160,
+                height: 160,
+                q: 50,
+                format: 'png'
+            });
+            baseInfo.avatar += "?" + Date.now();
+            callback(null, baseInfo);
+        });
+    });
+};
+
+User.updateUser = function(id, obj, callback) {
+    var self = this;
+    self.update({
+        "_id": id
+    }, {
+        $set: obj
+    }, function(err, rs) {
+        callback(err, rs);
+    });
+};
+
+User.saveBaseInfo = function(baseInfo, callback) {
+    var self = this;
+    if (!baseInfo.name || baseInfo.name.length < 2) {
+        return callback("名字最少需要两个字符");
+    }
+    self._initQiQiu();
+    self.existsByField("name", baseInfo.name, function(err, existsUser) {
+        if (err) {
+            return callback(err);
+        }
+        if (existsUser && existsUser._id != baseInfo.id) {
+            return callback('名字 "' + baseInfo.name + '" 已经被使用');
+        }
+        if (!baseInfo.avatar) {
+            return self.updateUser(baseInfo.id, {
+                name: baseInfo.name
+            }, function(err) {
+                callback(err, baseInfo);
+            });
+        }
+        self._uploadAvatar(baseInfo, function(err, info) {
+            self.updateUser(info.id, {
+                name: info.name,
+                avatar: info.avatar
+            }, function(err) {
+                callback(err, info);
+            });
+        });
+    });
+};
 
 module.exports = User;
