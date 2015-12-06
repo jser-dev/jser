148a7b7c2485eb5645b40dd3d5b13f7b71b658c3
Houfeng <admin@xhou.net>
2015-12-06 18:12:56 +0800

    dev

---
+++ api/topic.js
@@ -0,0 +1,11 @@
+var TopicController = function () { };
+
+TopicController.prototype.get = function () { };
+
+TopicController.prototype.post = function () { };
+
+TopicController.prototype.put = function () { };
+
+TopicController.prototype.delete = function () { };
+
+module.exports = TopicController;
