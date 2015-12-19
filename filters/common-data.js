117867605968791aba1e05b6b0b4174a16a3e755
Houfeng <admin@xhou.net>
2015-12-20 03:16:49 +0800

    添加消息提醒功能

--- filters/common-data.js
+++ filters/common-data.js
@@ -1,13 +1,26 @@
+/* global nokit */
 var User = require("../models/user");
+var Message = require("../models/message");
+var Task = nokit.Task;
 
 var CommonDataFilter = function () { };
 
 CommonDataFilter.prototype.onMvcHandle = function (context, next) {
-	User.getList(5, function (err, userList) {
+	var task = new Task();
+	task.add("topUserList", function (done) {
+		User.getList(5, done);
+	});
+	if (context.user) {
+		task.add("unreadMsgList", function (done) {
+			Message.getUnreadByUserId(context.user._id, done);
+		});
+	}
+	task.end(function (err, rs) {
 		if (err) {
 			return context.error(err);
 		}
-		context.topUserList = userList;
+		context.topUserList = rs.topUserList;
+		context.unreadMsgCount = rs.unreadMsgList.length;
 		next();
 	});
 };
