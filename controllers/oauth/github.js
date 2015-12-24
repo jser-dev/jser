732d4abb72b2bc892a8ef096db5b76594dd91ff0
Houfeng <admin@xhou.net>
2015-12-24 20:49:42 +0800

    dev

--- controllers/oauth/github.js
+++ controllers/oauth/github.js
@@ -1,10 +1,10 @@
 "use strict";
+/* global nokit */
 
 var request = require('request');
 var User = require('../../models/user');
 var utils = require("../../common/utils");
-
-/* global nokit */
+var Task = nokit.Task;
 
 var OAUTH_STATE_SESSION_KEY = "oauth_state";
 
@@ -32,8 +32,7 @@ GitHubController.prototype.index = function () {
     });
 };
 
-//github 回调
-GitHubController.prototype.callback = function () {
+GitHubController.prototype.getToken = function (callback) {
     var self = this;
     var code = self.context.data('code');
     var state = self.context.data('state');
@@ -44,48 +43,96 @@ GitHubController.prototype.callback = function () {
         request.post({
             "url": self.configs.token_url,
             "headers": {
-                "Accept": "application/json"
+                "Accept": "application/json",
+                "X-OAuth-Scopes": self.configs.scope,
+                "X-Accepted-OAuth-Scopes": self.configs.scope
             },
             "formData": {
                 "client_id": self.configs.client_id,
                 "client_secret": self.configs.client_secret,
                 "code": code,
                 "state": state
-                // ,
                 // "redirect_uri": self.configs.redirect_uri
             }
         }, function (err, httpResponse, body) {
-            if (err) {
-                return self.context.error(err);
-            }
             var tokenResult = JSON.parse(body);
-            var userInfoUrl = self.configs.user_url + "?access_token=" + tokenResult.access_token;
-            request({
-                "url": userInfoUrl,
-                "headers": {
-                    "User-Agent": "JSER"//Awesome-Octocat-App
-                }
-            }, function (err, httpResponse, body) {
-                if (err) {
-                    return self.context.error(err);
-                }
-                var userInfo = JSON.parse(body);
-                var user = new User();
-                user.email = userInfo.email;
-                user.name = "GitHub-" + (userInfo.login || userInfo.email);
-                user.avatar = userInfo.avatar_url;
-                user.password = utils.newGuid();
-                user.verifyCode = "";
-                User.oAuth(user, function (err, authedUser) {
-                    if (err) {
-                        return self.context.error(err);
-                    }
-                    self.context.session.set('user', authedUser, function () {
-                        self.session.get("referer", function (referer) {
-                            self.context.redirect(referer || "/");
-                        });
-                    });
-                });
+            callback(err, tokenResult);
+        });
+    });
+};
+
+GitHubController.prototype.getUserInfo = function (access_token, callback) {
+    var self = this;
+    var userInfoUrl = self.configs.user_url + "?access_token=" + access_token;
+    request({
+        "url": userInfoUrl,
+        "headers": {
+            "User-Agent": "JSER",//Awesome-Octocat-App,
+            "X-OAuth-Scopes": self.configs.scope,
+            "X-Accepted-OAuth-Scopes": self.configs.scope
+        }
+    }, function (err, httpResponse, body) {
+        var userInfo = JSON.parse(body);
+        callback(err, userInfo);
+    });
+};
+
+GitHubController.prototype.getEmail = function (access_token, callback) {
+    var self = this;
+    request({
+        "url": self.configs.email_url + "?access_token=" + access_token;
+        "headers": {
+            "User-Agent": "JSER",//Awesome-Octocat-App,
+            "X-OAuth-Scopes": self.configs.scope,
+            "X-Accepted-OAuth-Scopes": self.configs.scope
+        }
+    }, function (err, httpResponse, body) {
+        var emailInfo = JSON.parse(body);
+        callback(err, emailInfo);
+    });
+};
+
+//github 回调
+GitHubController.prototype.callback = function () {
+    var self = this;
+    var task = new Task();
+    task.add({
+        "getToken": function (done) {
+            self.getToken(done);
+        },
+        "getUserInfo": function (done) {
+            self.getUserInfo(task.result["getToken"].access_token, done);
+        },
+        "getEmail": function (done) {
+            var userInfo = task.result["getUserInfo"];
+            if (userInfo.email) {
+                done(null, userInfo);
+            } else {
+                self.getEmail(task.result["getToken"].access_token, done);
+            }
+        }
+    });
+    task.seq(function (err, result) {
+        var userInfo = result["fillEmail"];
+        self.loginUser(userInfo);
+    });
+};
+
+GitHubController.prototype.loginUser = function (userInfo) {
+    var self = this;
+    var user = new User();
+    user.email = userInfo.email;
+    user.name = "GitHub-" + (userInfo.login || userInfo.email);
+    user.avatar = userInfo.avatar_url;
+    user.password = utils.newGuid();
+    user.verifyCode = "";
+    User.oAuth(user, function (err, authedUser) {
+        if (err) {
+            return self.context.error(err);
+        }
+        self.context.session.set('user', authedUser, function () {
+            self.session.get("referer", function (referer) {
+                self.context.redirect(referer || "/");
             });
         });
     });
